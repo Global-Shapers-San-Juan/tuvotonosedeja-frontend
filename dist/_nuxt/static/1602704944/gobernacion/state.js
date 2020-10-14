2a16fe2f96ea4725b19de59b534b6f95fbcdc752
Pedro <pedro@rokailabs.com>
2020-10-14 18:07:55 -0400

    chore: add new dist folder

--- dist/_nuxt/static/1602704944/gobernacion/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1602704944",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgobernacion",config:{content:{dbHash:"ebd1d97d"},strapi:{url:"https:\u002F\u002Ftuvotonosedeja-api.herokuapp.com"}},apollo:{defaultClient:{}},strapi:{user:a}}}(null));
---
+++ dist/_nuxt/static/1602713182/senado/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1602713182",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsenado",config:{content:{dbHash:"be0d1aa8"},strapi:{url:"https:\u002F\u002Ftuvotonosedeja-api.herokuapp.com"}},apollo:{defaultClient:{}},strapi:{user:a}}}(null));
